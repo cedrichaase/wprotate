cacbd4e5ce532b1e96bdad0bceb513a6e86b752c
Cedric Haase <cedric@sineband.de>
2017-07-13 19:21:11 +0200

    Change xattr flag

--- wprotate.js
+++ wprotate.js
@@ -3,6 +3,8 @@ const xattr = require('fs-xattr');
 const fs = require('fs');
 const _ = require('lodash');
 
+const used_flag = 'wprotate.used';
+
 const wallpaper_path = '/Users/cedric/img/wallpapers';
 
 const allWallpapers = fs.readdirSync(wallpaper_path)
@@ -10,14 +12,14 @@ const allWallpapers = fs.readdirSync(wallpaper_path)
     .filter(x => fs.statSync(x).isFile());
 
 let unusedWallpapers = allWallpapers
-    .filter(x => xattr.listSync(x).indexOf('used') < 0);
+    .filter(x => xattr.listSync(x).indexOf(used_flag) < 0);
 
-if(unusedWallpapers.length === 0) {
-    allWallpapers.forEach(x => xattr.removeSync(x, 'used'));
+if (unusedWallpapers.length === 0) {
+    allWallpapers.forEach(x => xattr.removeSync(x, used_flag));
     unusedWallpapers = allWallpapers;
 }
 
 const newWallpaper = _.sample(unusedWallpapers);
 wallpaper.set(newWallpaper).then(() => {
-    xattr.setSync(newWallpaper, 'used', '1');
+    xattr.setSync(newWallpaper, used_flag, '1');
 });
