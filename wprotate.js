2065f703a783d1b945ef0ad70e5368950966476e
Cedric Haase <cedric@sineband.de>
2017-07-13 19:43:18 +0200

    Add some comments

--- wprotate.js
+++ wprotate.js
@@ -10,18 +10,22 @@ const used_flag = 'wprotate.used';
 const wallpaper_path = `${os.homedir()}/img/wallpapers`;
 const hooks_path = './hooks';
 
+// get wallpapers
 const allWallpapers = fs.readdirSync(wallpaper_path)
     .map(x => `${wallpaper_path}/${x}`)
     .filter(x => fs.statSync(x).isFile());
 
+// determine which ones have not been used yet
 let unusedWallpapers = allWallpapers
     .filter(x => xattr.listSync(x).indexOf(used_flag) < 0);
 
+// if all have been used, reset their used flags
 if (unusedWallpapers.length === 0) {
     allWallpapers.forEach(x => xattr.removeSync(x, used_flag));
     unusedWallpapers = allWallpapers;
 }
 
+// set the wallpaper and execute hooks
 const newWallpaper = _.sample(unusedWallpapers);
 wallpaper.set(newWallpaper).then(() => {
     xattr.setSync(newWallpaper, used_flag, '1');
